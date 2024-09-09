81116f5bb19f629b4b901d1ec0dc9a31e64c2a93
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2024-09-09 16:00:53 -0400

    Fixed issue where negative numbers were not being properly evaluated

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
+if number < 0:
+    last_digit = -abs(last_digit)
 if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit > 5:
