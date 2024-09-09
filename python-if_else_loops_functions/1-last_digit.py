dd72d94d0371e4955009588807158d31cc6b24f8
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2024-09-09 17:43:17 -0400

    Fixed negatives output

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if number < 0:
-    last_digit = -abs(last_digit)
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = number % -10
 if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit > 5:
