1df197a4c17c00bec5d8aca5edd9f6a04a64c439
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2024-09-10 19:19:38 -0400

    Fix on 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
-    if number < 99:
+    if number <= 98:
         print("{:02d}".format(number), end= ", ")
     else:
         print("{:02d}".format(number))
