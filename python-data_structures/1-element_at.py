a772a7298cc4b3ca99b7a5599bcd73362c370fee
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2024-09-16 19:37:44 -0400

    Combined line 3 and 5 in one

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
+    if idx < 0 and idx >= len(my_list):
         return None
     print(my_list(idx))
