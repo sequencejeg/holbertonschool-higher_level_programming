5bb3a0a071fd673a856dd26196f4adf1bf707fed
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2024-09-16 21:51:26 -0400

    Adding 6 7 8 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_int = my_list[0]
+    if len(my_list) > 0:
+        for i in my_list:
+            if i > max_int:
+                max_int = i
+    return max_int
