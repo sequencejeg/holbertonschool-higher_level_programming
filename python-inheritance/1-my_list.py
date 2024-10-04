f156fb0e227059ad9f7e2ffa7f66a78aaf6be0ee
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2024-10-03 22:34:09 -0400

    Added missing documentation on class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,14 @@ list class in Python.
 
 
 class MyList(list):
+    """
+    A custom list class that inherits
+    from the built-in list class.
+
+    This class provides an additional method, print_sorted,
+    which prints the elements of the list in sorted order.
+    """
+    
     def print_sorted(self):
         """
         The function `print_sorted` prints
